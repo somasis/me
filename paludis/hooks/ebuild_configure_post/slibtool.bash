67b3e6c1a8ce851a93a77cb609d6a7adadf97693
Kylie McClain <somasis@exherbo.org>
2016-05-21 12:45:16 -0400

    slibtool hook: remove

--- paludis/hooks/ebuild_configure_post/slibtool.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find -name libtool | while read f;do
-    echo "slibtool: writing to \"$f\"..." >&2
-    mv "$f" "$f".orig
-    printf '#!/bin/sh\nexec %s "$@"' "$(type -fPp slibtool)" > "$f"
-    chmod +x "$f"
-done
