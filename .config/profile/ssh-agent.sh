98d085be33049da13ea47d994c7afac7a0a471f1
Kylie McClain <kylie@somas.is>
2020-07-26 19:29:07 -0400

    profile/ssh-agent: make filename nicer for if XDG_RUNTIME_DIR is /tmp

--- .config/profile/ssh-agent.sh
+++ .config/profile/ssh-agent.sh
@@ -2,11 +2,14 @@
 # ssh-agent - Take in the ssh-agent(1) environment variables.
 
 # ...without creating duplicate agents!
-if ! [ -f "${XDG_RUNTIME_DIR}"/ssh-agent.sh ] \
+_ssh_agent_uid=$(id -u)
+if ! [ -f "${XDG_RUNTIME_DIR}"/ssh-agent-"${_ssh_agent_uid}".sh ] \
     || [ -z "${SSH_AGENT_PID}" ] \
     || ! kill -0 "${SSH_AGENT_PID}"; then
-    ssh-agent | grep -v '^echo' >"${XDG_RUNTIME_DIR}"/ssh-agent.sh
+    ssh-agent | grep -v '^echo' >"${XDG_RUNTIME_DIR}"/ssh-agent-"${_ssh_agent_uid}".sh
 fi
 
-. "${XDG_RUNTIME_DIR}"/ssh-agent.sh
+. "${XDG_RUNTIME_DIR}"/ssh-agent-"${_ssh_agent_uid}".sh
 ssh-add -q
+
+unset -v _ssh_agent_uid
