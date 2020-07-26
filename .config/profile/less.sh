944e1a54504f2ab7cfcaae43f3c768adb1681aba
Kylie McClain <kylie@somas.is>
2020-07-26 19:28:39 -0400

    profile/less: set some fancier LESS_TERMCAP variables

--- .config/profile/less.sh
+++ .config/profile/less.sh
@@ -2,13 +2,13 @@
 # less - See less(1) for details.
 
 # Colors for man pages
-export LESS_TERMCAP_mb=$(printf "\e[1;31m")
-export LESS_TERMCAP_md=$(printf "\e[1;31m")
+export LESS_TERMCAP_us=$(printf "\e[3;32m") # italicized in asciidoctor
+export LESS_TERMCAP_mb=$(printf "\e[3m")
+export LESS_TERMCAP_md=$(printf "\e[1;33m") # sections, and bolded in asciidoctor
 export LESS_TERMCAP_me=$(printf "\e[0m")
 export LESS_TERMCAP_se=$(printf "\e[0m")
-export LESS_TERMCAP_so=$(printf "\e[1;44;33m")
+export LESS_TERMCAP_so=$(printf "\e[0m")
 export LESS_TERMCAP_ue=$(printf "\e[0m")
-export LESS_TERMCAP_us=$(printf "\e[1;32m")
 
 # -s - combine multiple blank lines
 # -R - "Like -r, but only ANSI "color" escape sequences are output in "raw" form."
