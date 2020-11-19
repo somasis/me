b89c244b775923e063c9c656571b62148df401e4
Kylie McClain <kylie@somas.is>
2020-11-19 17:35:10 -0500

    profile/toolchain: remove, unnecessary

--- etc/profile/toolchain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# toolchain - Set defaults for toolchain variables.
-
-if grep -q '^ID=arch$' /etc/os-release; then
-    export CC="clang"
-    export CXX="clang++"
-    export LD="ld.lld"
-fi
-
