f6f97cb0a5699058e5b083ede5458c8d9a719dc4
Kylie McClain <kylie@somas.is>
2020-12-03 19:50:15 -0500

    ssh-agent: don't get so fancy... and fix the variable name :|

--- etc/profile/ssh-agent.sh
+++ etc/profile/ssh-agent.sh
@@ -2,4 +2,4 @@
 # ssh-agent - Set ssh-agent(1) environment variables.
 # The agent itself is expected to be managed by catsitd-user(8).
 
-export SSH_AGENT_SOCK="${XDG_RUNTIME_DIR}/ssh/agent"
+export SSH_AUTH_SOCK="${TMPDIR:-/tmp}"/ssh-agent-"${USER}"
