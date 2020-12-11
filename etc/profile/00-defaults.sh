732b701197c1470448c0366154ba674697a9232d
Kylie McClain <kylie@somas.is>
2020-12-10 19:49:19 -0500

    profile/00-defaults: expand $PAGER

--- etc/profile/00-defaults.sh
+++ etc/profile/00-defaults.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # 00-defaults - Default application variables
 
-command -v lunch >/dev/null 2>&1 && eval "$(lunch -s)"
+# Expand PAGER when taking in lunch(1) variables,
+# to fix stuff like mandoc doing special things for less(1).
+command -v lunch >/dev/null 2>&1 \
+    && eval "$(lunch -s)" \
+    && export PAGER="$(lunch -g pager | cut -d' ' -f1)"
