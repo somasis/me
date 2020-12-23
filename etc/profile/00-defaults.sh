aad8fa608f0f1a4c63e072fa84554f7e74fc1b2c
Kylie McClain <kylie@somas.is>
2020-12-23 01:48:38 -0500

    defaults: set a default PAGER and EDITOR if lunch isn't installed

--- etc/profile/00-defaults.sh
+++ etc/profile/00-defaults.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # 00-defaults - Default application variables
 
+# Defaults for if lunch isn't on the system.
+export EDITOR=vi PAGER=less
+command -v kak && export EDITOR=kak
+
 # Expand PAGER when taking in lunch(1) variables,
 # to fix stuff like mandoc doing special things for less(1).
 command -v lunch >/dev/null 2>&1 \
