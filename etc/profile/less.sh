f7dcc691b11f3acf97f5db1afdf1fe6087f72be6
Kylie McClain <kylie@somas.is>
2021-05-11 23:37:13 -0400

    profile/less: update options

--- etc/profile/less.sh
+++ etc/profile/less.sh
@@ -15,13 +15,18 @@ export LESS_TERMCAP_ue=$(printf '%b' "\033[0m")
 # -F - Quit if the whole thing can be viewed on one screen
 # -M - Show the lines and percentage of way through the input.
 # -R - "Like -r, but only ANSI "color" escape sequences are output in "raw" form."
-# -X - don't print the initialization screen- i.e, don't clear the terminal
 # --mouse - support mouse scrolling
 # --wheel-lines - only scroll 2 lines for each tick of the wheel
-export LESS='-isMFRX --mouse --wheel-lines=2'
+# --incsearch - search as you type
+#
+# Removed:
+#   -X - don't print the initialization screen- i.e, don't clear the terminal
+#        Using -X caused scrolling inconsistencies on xterm. Patched in less
+#        v581, but it seems like Arch does not have that version yet...
+#        <https://github.com/gwsw/less/issues/24>
+export LESS='-isMFR --mouse --wheel-lines=2'
 
 # XDG compliance
 export LESSHISTFILE="${XDG_CACHE_HOME}"/less/history
 export LESSKEY="${XDG_CONFIG_HOME}"/less/lesskey
 [ -d "${XDG_CACHE_HOME}"/less ] || mkdir -p "${XDG_CACHE_HOME}"/less
-
