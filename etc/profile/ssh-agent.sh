d51c3ea5c58ce2afc5bfffedf1c2513432a15bf1
Kylie McClain <kylie@somas.is>
2021-05-18 16:29:06 -0400

    ssh-agent: default to XDG_RUNTIME_DIR

--- etc/profile/ssh-agent.sh
+++ etc/profile/ssh-agent.sh
@@ -2,4 +2,4 @@
 # ssh-agent - Set ssh-agent(1) environment variables.
 # The agent itself is expected to be managed by catsitd-user(8).
 
-export SSH_AUTH_SOCK="${TMPDIR:-/tmp}"/ssh-agent-"${USER}"
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR:-${TMPDIR:-/tmp}}"/ssh-agent-"${USER}"
