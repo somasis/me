a78e98cdb3bd6e7149d9b8bcb60a0295a0dd3fc8
Kylie McClain <kylie@somas.is>
2021-05-18 16:59:24 -0400

    ksh/rc.d/prompt.ksh -> sh/rc.d/prompt.sh

--- etc/sh/rc.d/prompt.sh
+++ etc/sh/rc.d/prompt.sh
@@ -1,5 +1,5 @@
-#!/bin/ksh
-# prompt - Set variables such as $PS1. See ksh(1).
+#!/bin/sh
+# prompt - Set variables such as $PS1. See sh(1).
 
 PS1='\[\e[0m\]'
 
@@ -13,9 +13,9 @@ esac
 PS1="${PS1}"'\[$(echo -e "\033]0;${SSH_CONNECTION:+ssh [${USER}@${HOSTNAME}]: }${PWD}\007")\]'
 
 # Show hostname only over ssh(1) connections.
-[ -n "${SSH_CONNECTION}" ] && PS1="${PS1}"'@\[\e[0m\]\[\e[[35m\]\h\[\e[0m\]'
+[ -n "${SSH_CONNECTION}" ] && PS1="${PS1}"'@\[\e[0;35m\]\h\[\e[0m\]'
 
-PS1="${PS1}"' \[\e[1m\e[37m\]\w\[\e[0m\] '
+PS1="${PS1}"' \[\e[1;39m\]\w\[\e[0m\] '
 
 # Show exit status of last ran command.
 PS1="${PS1}"'\[\e[0m\]\[$([ "$?" -eq 0 ] && printf "\e[32m" || printf "\e[31m")\]●\[\e[0m\] '
