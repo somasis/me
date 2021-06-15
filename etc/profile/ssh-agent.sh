33e00c59ab4c64cd34b0baf4684cd7a9451052aa
Kylie McClain <kylie@somas.is>
2021-06-15 17:51:44 -0400

    ssh-agent: now managed by aur/systemd-ssh-agent

--- etc/profile/ssh-agent.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# ssh-agent - Set ssh-agent(1) environment variables.
-# The agent itself is expected to be managed by catsitd-user(8).
-
-export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR:-${TMPDIR:-/tmp}}"/ssh-agent-"${USER}"
