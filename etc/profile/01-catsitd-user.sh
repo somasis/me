ad1c29cff530c67f065fcdabfbb5e7e232f7d370
Kylie McClain <kylie@somas.is>
2021-06-21 00:06:25 -0400

    catsitd-user: add a 15 second delay between restarts

--- etc/profile/01-catsitd-user.sh
+++ etc/profile/01-catsitd-user.sh
@@ -5,6 +5,6 @@
 
 if ! [ -f "${XDG_RUNTIME_DIR}"/catsitd-user-"${USER}".pid ] \
     || ! kill -0 $(cat "${XDG_RUNTIME_DIR}"/catsitd-user-"${USER}".pid); then
-    catsitd-user -r 0
+    catsitd-user -r 15000
 fi
 
